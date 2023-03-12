8560e6867d727a1de62fa53f5b964622a1a5101f
Eric Handley <ehandley96@gmail.com>
2023-03-12 17:58:58 -0500

    Removed test endpoint

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('/user/name/available', views.username_available,
          name='user-name-available'),
     path('/user/login', views.user_login, name='user-login'),
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,12 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
-
 
 def username_available(request):
     return HttpResponse("Checked if username is available")
