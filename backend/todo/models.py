13b53deb07d1056d9f7fda3cdbc997ba5952eb8c
Eric Handley <ehandley96@gmail.com>
2023-03-12 15:59:43 -0500

    Created models for User, Task, and Task_Share tables

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    hashed_pw = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_created = models.DateTimeField()
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User,
+    )
+    task_name = models.CharField(max_length=255)
+    task_description = models.CharField(max_length=4095)
+    color = models.CharField(max_length=6)
+    date_created = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+
+class Task_Share(models.Model):
+    task = models.ForeignKey(
+        Task,
+    )
+    sender_user = models.ForeignKey(
+        User,
+    )
+    recipient_user = models.ForeignKey(
+        User,
+    )
+    date_shared = models.DateTimeField()
+    viewed = models.BooleanField()
