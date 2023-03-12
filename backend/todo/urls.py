76436c5a97deda97e131c6e758ee67e4f2912ba8
Eric Handley <ehandley96@gmail.com>
2023-03-12 17:57:57 -0500

    Created skeleton views and added them to urls.py for organization's sake.

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/user/name/available', views.username_available,
+         name='user-name-available'),
+    path('/user/login', views.user_login, name='user-login'),
+    path('/task/list', views.task_list, name='task-list'),
+    path('/task/new', views.task_create, name='task-create'),
+    path('/task/:id/edit', views.task_edit, name='task-edit'),
+    path('/task/:id/delete', views.task_delete, name='task-delete'),
 ]
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -2,5 +2,35 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the todo index.")
+
+
+def username_available(request):
+    return HttpResponse("Checked if username is available")
+
+
+def user_login(request):
+    return HttpResponse("Logged user in")
+
+
+def task_list(request):
+    return HttpResponse("Got the task list")
+
+
+def task_create(request):
+    return HttpResponse("Created a task")
+
+
+def task_delete(request):
+    return HttpResponse("Deleted a task")
+
+
+def task_edit(request):
+    return HttpResponse("Edited a task")
+
+
+def share_task(request):
+    return HttpResponse("Shared the task list")
