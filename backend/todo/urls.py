b335da11a8cdc5e52f445b89da47f49b3701a26b
Eric Handley <ehandley96@gmail.com>
2023-03-14 01:06:45 -0500

    Fixed urls.py to handle parameters (task_id). Implemented /task/:id/delete

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,16 +1,37 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('user/name/available', views.UsernameAvailabilityView.is_available,
-         name='user-name-available'),
-    path('user/login', views.user_login, name='user-login'),
-    path('task/list', views.TaskListView.get, name='task-list'),
-    path('task/new', views.TaskCreateView.create, name='task-create'),
-    path(r'task/(?P<task_id>[0-9])/edit', views.task_edit, name='task-edit'),
-    path(r'task/(?P<task_id>[0-9])/delete',
-         views.task_delete, name='task-delete'),
+    path(
+        'user/name/available',
+        views.UsernameAvailabilityView.is_available,
+        name='user-name-available'
+    ),
+    path(
+        'user/login',
+        views.user_login,
+        name='user-login'
+    ),
+    path(
+        'task/list',
+        views.TaskListView.get,
+        name='task-list'
+    ),
+    path(
+        'task/new',
+        views.TaskCreateView.create,
+        name='task-create'
+    ),
+    re_path(
+        r'task/(?P<task_id>[0-9])/edit',
+        views.task_edit,
+        name='task-edit',
+    ),
+    re_path(
+        r'task/(?P<task_id>[0-9])/delete',
+        views.TaskDeleteView.delete,
+        name='task-delete'),
 ]
 
 # Need to format this better. Want to add save-on-edit rule to put one function parameter per line when you append ',' to the last
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -12,12 +12,12 @@ class UsernameAvailabilityView(ListView):
     @csrf_exempt
     def is_available(request):
         if request.method == 'POST':
-            username_to_check = request.POST.get("username")
+            username_to_check = request.POST.get('username')
 
             if not username_to_check:
                 # Bad request. No username came with the request. Send back a 422 with message
                 response_body = {
-                    "message": "Error! No username submitted with request"
+                    'message': 'Error! No username submitted with request'
                 }
                 return JsonResponse(response_body, status=422)
 
@@ -28,7 +28,7 @@ class UsernameAvailabilityView(ListView):
             if not acceptable_name_format:
                 # username is not an acceptable format, so return a 422 with message
                 response_body = {
-                    "message": "The username is not in the valid format"
+                    'message': 'The username is not in the valid format'
                 }
                 return JsonResponse(response_body, status=422)
             else:
@@ -39,13 +39,13 @@ class UsernameAvailabilityView(ListView):
                 username_is_available = len(account_matching_username) == 0
 
                 response_body = {
-                    "message": "Successfully checked availability",
-                    "available": bool(username_is_available)
+                    'message': 'Successfully checked availability',
+                    'available': bool(username_is_available)
                 }
                 return JsonResponse(response_body, status=200)
         else:
             response_body = {
-                "message": "Endpoint does not exists for this HTTP verb"
+                'message': 'Endpoint does not exists for this HTTP verb'
             }
             return JsonResponse(response_body, status=404)
 
@@ -53,31 +53,31 @@ class UsernameAvailabilityView(ListView):
 
 
 def user_login(request):
-    return HttpResponse("Logged user in")
+    return HttpResponse('Logged user in')
 
 
 class TaskListView(ListView):
     def get(request):
-        if request.method == "GET":
+        if request.method == 'GET':
             # Fetch tasks from the DB and except any errors
             try:
                 task_list = list(Task.objects.values())
 
                 response_body = {
-                    "message": "Success",
-                    "tasks": task_list,
+                    'message': 'Success',
+                    'tasks': task_list,
                 }
 
                 return JsonResponse(response_body, status=200)
             except Exception as error:
                 response_body = {
-                    "message": f"An error occurred retrieving tasks: {error}",
-                    "tasks": None
+                    'message': f'An error occurred retrieving tasks: {error}',
+                    'tasks': None
                 }
                 return JsonResponse(response_body, status=500)
         else:
             response_body = {
-                "message": "Endpoint does not exists for this HTTP verb"
+                'message': 'Endpoint does not exists for this HTTP verb'
             }
             return JsonResponse(response_body, status=404)
 
@@ -91,23 +91,23 @@ class TaskCreateView(ListView):
             task_data = dict(request.POST.items())
 
             print(task_data)
-            new_task = Task(user_id=task_data["user_id"],
-                            task_name=task_data["name"],
-                            task_description=task_data["description"],
-                            color=task_data["color"],
-                            date_created=task_data["date_created"],
-                            start_date=task_data["start_date"],
-                            end_date=task_data["end_date"]
+            new_task = Task(user_id=task_data['user_id'],
+                            task_name=task_data['name'],
+                            task_description=task_data['description'],
+                            color=task_data['color'],
+                            date_created=task_data['date_created'],
+                            start_date=task_data['start_date'],
+                            end_date=task_data['end_date']
                             )
             new_task.save()
 
             response_body = {
-                "message": ""
+                'message': 'Successfully created task'
             }
             return JsonResponse(response_body, status=201)
         else:
             response_body = {
-                "message": "Endpoint does not exists for this HTTP verb"
+                'message': 'Endpoint does not exists for this HTTP verb'
             }
             return JsonResponse(response_body, status=404)
 
@@ -116,22 +116,68 @@ class TaskCreateView(ListView):
 
 class TaskDeleteView(ListView):
     @csrf_exempt
-    def delete(request):
+    def delete(request, task_id=0):
         # Delete a task
-        pass
+        if request.method == 'DELETE':
+            try:
+                task_to_delete = Task.objects.filter(id=task_id)
+            except Exception as Error:
+                # Failed to query for resource
+                return JsonResponse({'message': 'Error deleting task!'}, status=500)
+
+            # Check to make sure only one task was found
+            task_count = len(list(task_to_delete))
+
+            if task_count > 1:
+                # More than one task found for one pk. Throw an error and don't try to delete
+                response_body = {
+                    'message': 'Error! More than one task found while trying to delete a task'
+                }
+                return JsonResponse(response_body, status=500)
+
+            elif task_count == 0:
+                # No records found, so nothing to delete
+                response_body = {
+                    'message': 'No task found!'
+                }
+                return JsonResponse(response_body, status=404)
+
+            else:
+                # One record found as expected, so attempt to delete the resource
+                try:
+                    task_to_delete.delete()
+
+                    response_body = {
+                        'message': 'Successfully deleted task'
+                    }
+
+                    return JsonResponse(response_body, status=200)
+                except Exception as error:
+                    # failed to delete resource
+                    return JsonResponse({'message': 'Error deleting task!'}, status=500)
+
+        else:
+            response_body = {
+                'message': 'Endpoint does not exists for this HTTP verb'
+            }
+            return JsonResponse(response_body, status=404)
 
 
 def task_delete(request):
-    return HttpResponse("Deleted a task")
+    return HttpResponse('Deleted a task')
 
 # TODO: convert to class-based view
 
 
 def task_edit(request):
-    return HttpResponse("Edited a task")
+    if request.method == 'PUT':
+        updates = dict(request.POST.items())
+
+        # TODO: validate updates
+        pass
 
 # TODO: convert to class-based view
 
 
 def share_task(request):
-    return HttpResponse("Shared the task list")
+    return HttpResponse('Shared the task list')
